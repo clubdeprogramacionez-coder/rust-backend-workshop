8ecad788880f346bd25bccc8146bc9247e122ca7
Darick Y Gonzalez M <109446952+AeroScript-DYGM@users.noreply.github.com>
2026-08-20 10:14:58 -0600

    Update mod.rs

--- src/labs/mod.rs
+++ src/labs/mod.rs
@@ -1,31 +1,44 @@
 #[path = "1-lab-stack-heap.rs"]
-mod lab_1_stack_heap;
-#[path = "2.lab-ownership.rs"]
-mod lab_2_ownership;
-#[path = "3-lab-borrowing.rs"]
-mod lab_3_borrowing;
-#[path = "4-lab-slices-analizador.rs"]
-mod lab_4_slices_analizador;
-#[path = "5-lab-structs-impl.rs"]
-mod lab_5_structs_impl;
-#[path = "6-lab-enums-match.rs"]
-mod lab_6_enums_match;
-#[path = "7-lab-option.rs"]
-mod lab_7_option;
-#[path = "8-lab-result-calculadora.rs"]
-mod lab_8_result_calculadora;
-#[path = "9-lab-traits-basico.rs"]
-mod lab_9_traits_basico;
-#[path = "10-traits-estandar.rs"]
-mod lab_10_traits_estandar;
-#[path = "11-lab-genericos.rs"]
-mod lab_11_genericos;
-#[path = "12-lab-dyn-trait-jsonifiable.rs"]
-mod lab_12_dyn_trait_jsonifiable;
-#[path = "13-lab-tcp-eco-server.rs"]
-mod lab_13_tcp_eco_server;
-#[path = "14-lab-http-parser.rs"]
-mod lab_14_http_parser;
-#[path = "15-lab-concurrencia-thereads.rs"]
-mod lab_15_concurrencia_thereads;
+pub mod lab_1_stack_heap;
 
+#[path = "2.lab-ownership.rs"]
+pub mod lab_2_ownership;
+
+#[path = "3-lab-borrowing.rs"]
+pub mod lab_3_borrowing;
+
+#[path = "4-lab-slices-analizador.rs"]
+pub mod lab_4_slices_analizador;
+
+#[path = "5-lab-structs-impl.rs"]
+pub mod lab_5_structs_impl;
+
+#[path = "6-lab-enums-match.rs"]
+pub mod lab_6_enums_match;
+
+#[path = "7-lab-option.rs"]
+pub mod lab_7_option;
+
+#[path = "8-lab-result-calculadora.rs"]
+pub mod lab_8_result_calculadora;
+
+#[path = "9-lab-traits-basico.rs"]
+pub mod lab_9_traits_basico;
+
+#[path = "10-traits-estandar.rs"]
+pub mod lab_10_traits_estandar;
+
+#[path = "11-lab-genericos.rs"]
+pub mod lab_11_genericos;
+
+#[path = "12-lab-dyn-trait-jsonifiable.rs"]
+pub mod lab_12_dyn_trait_jsonifiable;
+
+#[path = "13-lab-tcp-eco-server.rs"]
+pub mod lab_13_tcp_eco_server;
+
+#[path = "14-lab-http-parser.rs"]
+pub mod lab_14_http_parser;
+
+#[path = "15-lab-concurrencia-thereads.rs"]
+pub mod lab_15_concurrencia_thereads;
